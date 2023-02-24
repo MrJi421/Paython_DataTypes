c17b756b7fded2a4697b354bdbd2417f3051f4b6
Hemant Kumar <121115151+MrJi421@users.noreply.github.com>
2023-02-24 20:13:02 +0530

    Create Test.c

---
+++ Test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conion>
+void main(){
+  int a,b,c,d;
+  printf("\nEnter the value of a: ");
+  scanf("%d",&a);
+   printf("\nEnter the value of b: ");
+  scanf("%d",&b);
+  c=a+b;
+   printf("\n the value of c=%d ",c);
+ d=a-b;
+   printf("\n the value of d=%d ",d);
+}
